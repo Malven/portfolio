feee2fea05fc07b09216646e74e304f3bad56d89
Marcus <Malven@users.noreply.github.com>
2021-05-16 11:51:37 +0200

    more updates

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,9 @@ import React from 'react';
 export default function Index() {
 	return (
 		<div>
-			<p>Hello Next.js</p>
+			<p className='text-center m-4 text-white font-bold border border-blue-600 w-1/2 p-4 bg-blue-500 rounded'>
+				Hello Next.js
+			</p>
 		</div>
 	);
 }
